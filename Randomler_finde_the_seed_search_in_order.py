d7c1b65ee570220a9bb011b4ee81b448fef95474
MakarovDs777 <42496353+MakarovDs777@users.noreply.github.com>
2025-02-24 08:52:35 +0300

    Update Randomler_finde_the_seed_search_in_order.py

--- Randomler_finde_the_seed_search_in_order.py
+++ Randomler_finde_the_seed_search_in_order.py
@@ -12,10 +12,10 @@ def find_seed(target_array):
         generated_array = generate_random_array(seed, length)
         # Вывод информации о текущем сидe и результате сравнения
         if generated_array == target_array:
-            print(f"{seed} 1")  # Сид соответствует
+            print(f"{seed} 1 {generated_array}")  # Сид соответствует
             return seed
         else:
-            print(f"{seed} 0")  # Сид не соответствует
+            print(f"{seed} 0 {generated_array}")  # Сид не соответствует
     return None  # Если сид не найден
 
 # Пример использования
@@ -27,3 +27,4 @@ if found_seed is not None:
     print(f"Найден сид: {found_seed}")
 else:
     print("Сид не найден.")
+
